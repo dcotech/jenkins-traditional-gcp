82e6b3ec7968153f6451c0136756aaba8653a4c9
Damien ONeal <doneal@cloudbees.com>
2020-05-06 11:12:03 -0500

    added instance.tf functionality and cleaned up modules

--- main.tf
+++ main.tf
@@ -9,16 +9,17 @@ module "vpc" {
   department             = "${var.department}"
   uc1_public_subnet      = "${var.uc1_public_subnet}"
   uc1_private_subnet     = "${var.uc1_private_subnet}"
-  region_name = "${var.region_name}"
+  region_name            = "${var.region_name}"
 }
 
-module "gce_instance" {
+module "instance" {
   source = "./modules/gce_instance"
   env    = "${var.env}"
   department = "${var.department}"
   #network_self_link = "${module.vpc.out_vpc_self_link}"
-  #subnetwork1 = "${module.gce_instance.uc1_out_public_subnet_name}"
-  uc1_public_subnet = "${var.uc1_public_subnet}"
-  uc1_private_subnet = "${var.uc1_private_subnet}"
+  subnetwork = "${module.vpc.uc1_out_public_subnet_name}"
+  #uc1_public_subnet = "${var.uc1_public_subnet}"
+  #uc1_private_subnet = "${var.uc1_private_subnet}"
   region_name = "${var.region_name}"
+  instance_size          = "${var.instance_size}"
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,31 @@
 #---- global/VPC variables
 
 variable "var_project" {
-        default = "ps-dev-201405"
-    }
+    default = "ps-dev-201405"
+}
+
 variable "env" {
-        default = "dev"
-    }
+    default = "dev"
+}
+
 variable "department" { 
-        default = "dco-cloudbees"
-    }
+    default = "dco-cloudbees"
+}
 
 #---- gce_instance/network variables
 
 variable "uc1_private_subnet" {
-        default = "10.26.1.0/24"
-    }
+    default = "10.26.1.0/24"
+}
 
 variable "uc1_public_subnet" {
-        default = "10.26.2.0/24"
-    }
+    default = "10.26.2.0/24"
+}
 
 variable "region_name" {
-        default = "us-east1"
+    default = "us-east1"
+}
+
+variable "instance_size" {
+    default = "n1-standard-1"
 }
